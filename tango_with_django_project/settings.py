50bc8862df9305b7501da33580504843dc4da5e7
hanyedeleng <hanyedeleng@gmail.com>
2016-05-21 22:29:48 -0400

    add registration, login and logout

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -85,7 +85,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
@@ -103,3 +102,6 @@ TEMPLATE_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+LOGIN_URL = '/rango/login/'
